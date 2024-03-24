2ab4fc4a87a8f364177756da84c21a4fc3eab4f1
Urich-loic <655808580ab@gmail.com>
2024-03-24 02:39:29 +0100

    margin up dated

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {<AcmeLogo />}
       </div>
-      <div className="mt-3 flex grow flex-col gap-4 md:flex-row">
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
